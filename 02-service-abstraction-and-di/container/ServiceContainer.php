e6ae3a3d53f3b615e23cfa4ada3c8a40abdd62b8
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-06 10:21:05 -0300

    Update ServiceContainer.php

--- 02-service-abstraction-and-di/container/ServiceContainer.php
+++ 02-service-abstraction-and-di/container/ServiceContainer.php
@@ -1,1 +1,19 @@
+<?php 
 
+namespace App\Container;
+
+class ServiceContainer
+ { 
+    private array $bindings = [];
+
+public function bind(string $abstract, callable $factory): void {
+        $this->bindings[$abstract] = $factory;
+    }
+
+    public function get(string $abstract) { 
+        if (!isset($this->bindings[$abstract])) { 
+            throw new \Exception("Serviço não registrado: {abstract} ");
+        } 
+        return $this->bindings[$abstract]($this);
+        }
+    }
