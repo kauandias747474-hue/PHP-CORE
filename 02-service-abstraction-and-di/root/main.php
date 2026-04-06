2243a9c636f2768aa2ff958d8fbd528547114a7b
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-06 10:27:29 -0300

    Update main.php

--- 02-service-abstraction-and-di/root/main.php
+++ 02-service-abstraction-and-di/root/main.php
@@ -1,1 +1,25 @@
+<?php
 
+require_once __DIR__ . '/vendor/autoload.php';
+
+use App\Container\ServiceContainer;
+use App\Implementations\EmailNotification;
+use App\Implementations\FileLogger;
+
+$container = new ServiceContainer();
+
+
+$container->bind('App\Contracts\ILogger', function($c) {
+    return new FileLogger();
+});
+
+$container->bind('App\Contracts\INotificationService', function($c) {
+    $logger = $c->get('App\Contracts\ILogger');
+    return new EmailNotification($logger);
+});
+
+
+$mensagem = $_GET['mensagem'] ?? "Teste Final";
+$notification = $container->get('App\Contracts\INotificationService');
+
+echo $notification->send($mensagem);
