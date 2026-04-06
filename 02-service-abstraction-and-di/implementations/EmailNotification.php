7c0bc8745751800a5e8b66bd20fa5b970c2a1e2b
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-06 10:24:09 -0300

    Update EmailNotification.php

--- 02-service-abstraction-and-di/implementations/EmailNotification.php
+++ 02-service-abstraction-and-di/implementations/EmailNotification.php
@@ -1,1 +1,23 @@
+<?php
 
+namespace App\Implementations;
+
+use App\Contracts\INotificationService;
+use App\Contracts\ILogger;
+
+class EmailNotification implements INotificationService
+{
+    private ILogger $logger;
+
+    public function __construct(ILogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    
+    public function send(string $message): string
+    {
+        $this->logger->log("Enviando e-mail: " . $message);
+        return "E-mail enviado com sucesso: " . $message;
+    }
+}
