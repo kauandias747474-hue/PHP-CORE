ceaed5d5896f49579664b5640afec449ce733ce3
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-06 10:28:14 -0300

    Update SmsNotification.php

--- 02-service-abstraction-and-di/implementations/SmsNotification.php
+++ 02-service-abstraction-and-di/implementations/SmsNotification.php
@@ -1,14 +1,30 @@
 <?php
 
 namespace App\Implementations;
+
 use App\Contracts\INotificationService;
+use App\Contracts\ILogger;
 
 class SmsNotification implements INotificationService
-{ 
-    public function send(string $message): void 
-    { 
+{
+    private ILogger $logger;
+    private const MAX_LENGTH = 160;
 
-    echo "Enviando SMS" . $message . "\n";
+    public function __construct(ILogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+
+    public function send(string $message): string
+    {
+        if (strlen($message) > self::MAX_LENGTH) {
+            $this->logger->log("ALERTA: SMS muito longo. Cortando mensagem.");
+            $message = substr($message, 0, self::MAX_LENGTH);
+        }
+
+        $this->logger->log("SMS Enviado: " . $message);
+        
+        return "Sucesso: SMS enviado (" . strlen($message) . " caracteres)";
     }
 }
-
