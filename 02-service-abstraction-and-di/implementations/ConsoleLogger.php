541137c708c0369fc64bbfd09984298b81f6c6b5
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-06 10:23:26 -0300

    Update ConsoleLogger.php

--- 02-service-abstraction-and-di/implementations/ConsoleLogger.php
+++ 02-service-abstraction-and-di/implementations/ConsoleLogger.php
@@ -1,1 +1,11 @@
+<?php 
 
+namespace App\Implementations;
+
+use App\Contracts\ILogger;
+
+class ConsoleLogger implements ILogger {
+    public function log(string $message): void { 
+        echo "[LOG - " . date('Y-m-d H:i:s') . "]: message". PHP_EOL;
+    }
+}
