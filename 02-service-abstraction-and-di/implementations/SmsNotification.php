6dfa5b37553b31359930b80af24a77b549a78e4f
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-06 10:24:24 -0300

    Update SmsNotification.php

--- 02-service-abstraction-and-di/implementations/SmsNotification.php
+++ 02-service-abstraction-and-di/implementations/SmsNotification.php
@@ -1,1 +1,14 @@
+<php
+
+namespace App\Implementations;
+use App\Contracts\INotificationService;
+
+class SmsNotification implements INotificationService
+{ 
+    public function send(string $message): void 
+    { 
+
+    echo "Enviando SMS" . $message . "\n";
+    }
+}
 
