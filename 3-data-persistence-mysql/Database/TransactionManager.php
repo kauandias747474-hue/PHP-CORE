83bc85dd324a98782e935ffbd7cb4d4fc602758e
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-09 09:33:18 -0300

    Update TransactionManager.php

--- 3-data-persistence-mysql/Database/TransactionManager.php
+++ 3-data-persistence-mysql/Database/TransactionManager.php
@@ -1,1 +1,23 @@
+<?php
+namespace App\Database;
 
+use PDO;
+
+class TransactionManager {
+    private $pdo;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function execute(callable $callback) {
+        try {
+            $this->pdo->beginTransaction();
+            $callback($this->pdo);
+            $this->pdo->commit();
+        } catch (\Exception $e) {
+            $this->pdo->rollBack();
+            throw $e;
+        }
+    }
+}
