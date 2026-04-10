91757cc24bdabf6dd8bbf61bc2c87d78b997a5d9
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-10 10:01:47 -0300

    Update SqlUserRepository.php

--- 3-data-persistence-mysql/Repositories/SqlUserRepository.php
+++ 3-data-persistence-mysql/Repositories/SqlUserRepository.php
@@ -1,1 +1,17 @@
+<?php
 
+class UserRepository 
+{ 
+    public function __construct(private readonly PDO $db) {} 
+
+    public function find(int $id): ?array 
+    {
+        if ($id <= 0) return null;
+
+        $stmt = $this->db->prepare("SELECT * FROM users WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $user ?: null;
+    }
+}
