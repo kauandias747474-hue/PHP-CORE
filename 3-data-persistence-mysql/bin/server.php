e5776b761053ed268119339c8e86f96e9a15b49f
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-11 11:10:34 -0300

    Update server.php

--- 3-data-persistence-mysql/bin/server.php
+++ 3-data-persistence-mysql/bin/server.php
@@ -1,1 +1,18 @@
+<?php
+$host = 'localhost:8000';
 
+$diretorioPublico = 'C:\Users\User\public';
+
+echo "--------------------------------------\n";
+echo "🚀 SERVIDOR WEB ATIVO\n";
+echo "Acesse: http://$host\n";
+echo "Alvo: $diretorioPublico\n";
+echo "--------------------------------------\n";
+
+if (!is_dir($diretorioPublico)) {
+    echo "[ERRO] Pasta 'public' nao encontrada em $diretorioPublico\n";
+    exit;
+}
+
+
+passthru("php -S $host -t $diretorioPublico");
