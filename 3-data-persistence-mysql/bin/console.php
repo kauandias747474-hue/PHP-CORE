3a9e5821829b4c2b449b1f4aed8cddd66e3e1329
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-11 11:10:47 -0300

    Update console.php

--- 3-data-persistence-mysql/bin/console.php
+++ 3-data-persistence-mysql/bin/console.php
@@ -1,1 +1,13 @@
+<?php
+echo "=== TESTANDO CONEXAO COM MYSQL ===\n";
 
+$dsn = "mysql:host=localhost;port=3307;dbname=modulo3php;charset=utf8mb4";
+$usuario = "root";
+$senha = "";
+
+try {
+    $db = new PDO($dsn, $usuario, $senha, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo "[OK] Conectado ao banco na porta 3307 com sucesso!\n";
+} catch (Exception $e) {
+    echo "[ERRO] Falha: " . $e->getMessage() . "\n";
+}
