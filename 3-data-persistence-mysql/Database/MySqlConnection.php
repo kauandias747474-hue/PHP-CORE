e19432d16a72545669bb73f8b7364da3257d0715
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-11 10:39:07 -0300

    Update MySqlConnection.php

--- 3-data-persistence-mysql/Database/MySqlConnection.php
+++ 3-data-persistence-mysql/Database/MySqlConnection.php
@@ -1,1 +1,31 @@
+<?php
 
+class MySqlConnection 
+{ 
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "modulo3php";
+    private $port = "3307"; // Porta ajustada para o seu XAMPP
+    
+    private static $instance = null;
+
+   
+    public static function getConnection() {
+        if (self::$instance === null) {
+            try {
+           
+                $dsn = "mysql:host=localhost;port=3307;dbname=modulo3php;charset=utf8mb4";
+                
+                self::$instance = new PDO($dsn, 'root', '', [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+                ]);
+            } catch (PDOException $e) {
+               
+                throw new Exception("Erro de conexão (Banco): " . $e->getMessage());
+            }
+        }  
+        return self::$instance;
+    }
+}
