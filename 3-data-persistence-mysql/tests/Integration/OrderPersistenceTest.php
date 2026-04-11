7c65c1be04669154f89e1aa6c5042e76edf457d3
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-11 12:17:23 -0300

    Update OrderPersistenceTest.php

--- 3-data-persistence-mysql/tests/Integration/OrderPersistenceTest.php
+++ 3-data-persistence-mysql/tests/Integration/OrderPersistenceTest.php
@@ -1,1 +1,14 @@
+<?php
 
+class OrderRepository {
+    private $lastValue;
+
+    public function save($value) {
+        $this->lastValue = $value;
+        return true; 
+    }
+
+    public function getLastSavedValue() {
+        return $this->lastValue;
+    }
+}
