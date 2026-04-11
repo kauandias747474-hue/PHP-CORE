b718d9b33aca4fabffe1601b4e9393328402fed2
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-11 11:25:56 -0300

    Update clear_logs.php

--- 3-data-persistence-mysql/storage/Unit/clear_logs.php
+++ 3-data-persistence-mysql/storage/Unit/clear_logs.php
@@ -1,1 +1,16 @@
+<?php
 
+echo "--- INICIANDO LIMPEZA DE LOGS ---\n";
+
+$diretorioLogs = __DIR__ . '/../logs';
+
+if (is_dir($diretorioLogs)) {
+    $arquivos = glob("$diretorioLogs/*.log"); // Pega todos os arquivos .log
+    foreach ($arquivos as $arquivo) {
+        unlink($arquivo); 
+        echo "Deletado: " . basename($arquivo) . "\n";
+    }
+    echo " Limpeza concluída.\n";
+} else {
+    echo " Pasta de logs não encontrada.\n";
+}
