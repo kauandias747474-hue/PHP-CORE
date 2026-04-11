2f0b4a2ca4b7886b4d858ca9d4a3652e91ed1603
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-11 12:17:10 -0300

    Update PaymentFlowTest.php

--- 3-data-persistence-mysql/tests/Integration/PaymentFlowTest.php
+++ 3-data-persistence-mysql/tests/Integration/PaymentFlowTest.php
@@ -1,1 +1,28 @@
+<?php
 
+class PaymentProcessor {
+    
+    public function handle($data) {
+        $timestamp = date('Y-m-d H:i:s');
+        $jsonPayload = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $logEntry = "[$timestamp] Transação processada: $jsonPayload" . PHP_EOL;
+        
+        $logPath = __DIR__ . '/clearing.log';
+
+        $resultado = file_put_contents($logPath, $logEntry, FILE_APPEND | LOCK_EX);
+        
+        if ($resultado === false) {
+            throw new Exception("Falha ao escrever log");
+        }
+
+        return true;
+    }
+
+    public function clearLogs() {
+        $logPath = __DIR__ . '/clearing.log';
+        if (file_exists($logPath)) {
+            return unlink($logPath);
+        }
+        return false;
+    }
+}
