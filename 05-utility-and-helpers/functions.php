6b24c741a49cb749eec6f1bcc031511482a5b10d
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 09:49:57 -0300

    Update functions.php

--- 05-utility-and-helpers/functions.php
+++ 05-utility-and-helpers/functions.php
@@ -1,1 +1,12 @@
+<?php
 
+function dd($data) {
+    echo "<pre style='background:#222; color:#0f0; padding:20px;'>";
+    var_dump($data);
+    echo "</pre>";
+    die();
+}
+
+function formatDate($date) {
+    return date('d/m/Y', strtotime($date));
+}
