a848fadcef15f8dce30bb767427f5dc6688b3208
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 09:51:22 -0300

    Update CpfValidator.php

--- 05-utility-and-helpers/Validators/CpfValidator.php
+++ 05-utility-and-helpers/Validators/CpfValidator.php
@@ -1,1 +1,11 @@
+<?php
+namespace App\ValueObjects;
 
+class Password {
+    public function __construct(private string $value) {
+        if (strlen($value) < 6) {
+            throw new \Exception("A senha deve ter pelo menos 6 caracteres!");
+        }
+    }
+    public function getValue(): string { return $this->value; }
+}
