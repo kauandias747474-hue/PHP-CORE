9b5ed0f00140e45e3e20c6f62d014f7f08b267fc
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 11:40:26 -0300

    Update functions.php

--- 06-infrastructure-services/functions.php
+++ 06-infrastructure-services/functions.php
@@ -1,1 +1,9 @@
+<?php
 
+
+function dd($data) {
+    echo "<pre style='background: #222; color: #5af78e; padding: 15px; border-radius: 5px;'>";
+    var_dump($data);
+    echo "</pre>";
+    die();
+}
