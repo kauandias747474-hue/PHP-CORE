5daea8aeb3534f1045972493297cd6b4181d51c2
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 16:02:48 -0300

    Update Authenticate.php

--- 08-api-and-routing/Middlewares/Authenticate.php
+++ 08-api-and-routing/Middlewares/Authenticate.php
@@ -1,1 +1,20 @@
+<?php
+namespace App\Middlewares;
 
+class Authenticate {
+    public function handle() {
+       
+        $token = $_GET['token'] ?? null;
+
+        if (!$token || $token !== 'secret123') {
+            http_response_code(401);
+            echo json_encode([
+                'error' => 'Não autorizado',
+                'message' => 'Você precisa de um token válido para acessar esta rota.'
+            ]);
+            exit; 
+        }
+        
+       
+    }
+}
