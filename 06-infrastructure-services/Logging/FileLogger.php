53a72232f9cd8d7f91b978c12442f8e5088cdc4d
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 11:37:55 -0300

    Update FileLogger.php

--- 06-infrastructure-services/Logging/FileLogger.php
+++ 06-infrastructure-services/Logging/FileLogger.php
@@ -1,1 +1,10 @@
+<?php
+namespace App\Logging;
 
+class FileLogger { 
+    public function log(string $message): void { 
+        $timestamp = date('Y-m-d H:i:s');
+        $formattedMessage = "[$timestamp] $message" . PHP_EOL;
+        file_put_contents('system.log', $formattedMessage, FILE_APPEND);
+    }
+}
