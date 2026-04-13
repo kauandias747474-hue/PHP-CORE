ec045d7f588263f4a9796279b226ecb548e28453
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 15:17:28 -0300

    Update user_profile.php

--- 07-frontend-integration/Views/user_profile.php
+++ 07-frontend-integration/Views/user_profile.php
@@ -1,1 +1,13 @@
+<?php use App\Assets\AssetHelper; ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Perfil do Usuário</title>
+    <link rel="stylesheet" href="<?= AssetHelper::css('style') ?>">
+</head>
+<body>
+    <h1>Olá, <?= $name ?>!</h1>
+    <img src="<?= AssetHelper::image('avatar.png') ?>" alt="Foto de Perfil">
+</body>
+</html>
