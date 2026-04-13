89e42b98e9ea338cdaf4ed123ff089be2267c242
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 15:17:41 -0300

    Update Renderer.php

--- 07-frontend-integration/ViewEngine/Renderer.php
+++ 07-frontend-integration/ViewEngine/Renderer.php
@@ -1,1 +1,15 @@
+<?php
+namespace App\ViewEngine;
 
+class Renderer {
+    public function render(string $view, array $data = []): void {
+        $viewPath = __DIR__ . "/../Views/{$view}.php";
+        
+        if (file_exists($viewPath)) {
+            extract($data); 
+            include $viewPath;
+        } else {
+            echo "Erro: View [{$view}] não encontrada.";
+        }
+    }
+}
