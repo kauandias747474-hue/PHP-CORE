be1f06b0f07d3a9a7af2c7c7c4452cc318dbed24
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 15:17:55 -0300

    Update JsonResponse.php

--- 07-frontend-integration/Responses/JsonResponse.php
+++ 07-frontend-integration/Responses/JsonResponse.php
@@ -1,1 +1,11 @@
+<?php
+namespace App\Responses;
 
+class JsonResponse {
+    public static function send(array $data, int $status = 200): void {
+        header('Content-Type: application/json');
+        http_response_code($status);
+        echo json_encode($data);
+        exit;
+    }
+}
