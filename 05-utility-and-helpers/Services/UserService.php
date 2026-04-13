ec826ad1d1a31308809ea5eeabaca9beec726bbd
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 09:52:09 -0300

    Update UserService.php

--- 05-utility-and-helpers/Services/UserService.php
+++ 05-utility-and-helpers/Services/UserService.php
@@ -1,1 +1,22 @@
+<?php
+namespace App\Services;
 
+use App\Entities\User;
+use App\Exceptions\DomainException;
+
+class UserService {
+   
+    public function register(User $user): bool {
+        // Exemplo de regra de negócio no Service:
+        if ($user->getName() === "Admin") {
+            throw new DomainException("Você não pode registrar um usuário com o nome 'Admin'.");
+        }
+
+        
+        return true; 
+    }
+
+    public function sendWelcomeEmail(User $user): void {
+        echo "📧 E-mail de boas-vindas enviado para: " . $user->getEmail()->getValue() . "<br>";
+    }
+}
