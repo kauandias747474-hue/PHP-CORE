c0365ae0cde258c9fdb1cfdd4c7043ecbcca2ba5
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 08:50:58 -0300

    Update DomainException.php

--- 04-business-domain-logic/Exceptions/DomainException.php
+++ 04-business-domain-logic/Exceptions/DomainException.php
@@ -1,1 +1,33 @@
+<?php
+namespace App\Exceptions;
+use Exception;
+use DateTimeImmutable;
 
+class DomainException extends Exception {
+    private ?string $field;
+    private string $severity;
+    private string $suggestion;
+    private DateTimeImmutable $timestamp;
+
+    public function __construct(
+        string $message, 
+        ?string $field = null, 
+        string $severity = 'LOW', 
+        string $suggestion = 'Verifique os dados.'
+    ) {
+        parent::__construct($message);
+        $this->field = $field;
+        $this->severity = $severity;
+        $this->suggestion = $suggestion;
+        $this->timestamp = new DateTimeImmutable();
+    }
+
+    public function getDetails(): array {
+        return [
+            'field'      => $this->field,
+            'severity'   => $this->severity,
+            'suggestion' => $this->suggestion,
+            'occurred_at'=> $this->timestamp->format('Y-m-d H:i:s')
+        ];
+    }
+}
