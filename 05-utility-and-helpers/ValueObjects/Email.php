dea62cdc2de553b5e1bd58d998141d9d36c75051
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 09:50:51 -0300

    Update Email.php

--- 05-utility-and-helpers/ValueObjects/Email.php
+++ 05-utility-and-helpers/ValueObjects/Email.php
@@ -1,1 +1,18 @@
+<?php
+namespace App\ValueObjects;
 
+use App\Exceptions\DomainException; 
+
+class Email {
+    private string $value;
+
+    public function __construct(string $value) {
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+          
+            throw new DomainException("O formato do e-mail '$value' é inválido.");
+        }
+        $this->value = $value;
+    }
+
+    public function getValue(): string { return $this->value; }
+}
