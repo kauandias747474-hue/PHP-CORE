fbbe078ce21e3f02de0f6e24bcfbaf438a8bd462
Kauan Oliveira <kauandias747474@gmail.com>
2026-04-13 15:20:44 -0300

    Update AssetHelper.php

--- 07-frontend-integration/Assets/AssetHelper.php
+++ 07-frontend-integration/Assets/AssetHelper.php
@@ -1,1 +1,18 @@
+<?php 
 
+namespace App\Assets;
+
+class AssetHelper {
+    private static $basePath = "Assets/";
+
+    public static function css(string $fileName): string {
+     
+        $fileName = str_replace('.css', '', $fileName);
+        
+        return self::$basePath . "css/" . $fileName . ".css";
+    }
+
+    public static function image(string $fileName): string {
+        return self::$basePath . "images/" . $fileName;
+    }
+} 
